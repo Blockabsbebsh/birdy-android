2c85e44d4824dc0fd0b62372a3c599e583066270
Gabrielius <317393049+Blockabsbebsh@users.noreply.github.com>
2026-08-31 11:49:48 +0300

    Release Birdy Android 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.blockabsbebsh.birdy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.1.0"
     }
 
     buildFeatures {
